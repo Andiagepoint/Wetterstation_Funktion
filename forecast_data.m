da5b6c6701b267b77f0c2d52ee364695ee63a309
Andreas Henneberger <andreas.henneberger@mytum.de>
2013-11-23 23:52:52 +0100

    New function forecast_data, gen_pdu added

---
+++ forecast_data.m
@@ -0,0 +1,29 @@
+function [ output_args ] = forecast_data( city, forecast_scope, forecast_detail, forecast_interval, automatic_call_interval , resolution )
+% Example
+% forecast_data('München','Niederschlag','Menge','Heute-Morgen-3.Folgetag-Abend','23-25-Nov-2013','1')
+%   Detailed explanation goes here
+
+% General variable settings
+fcode_read_sr   = '03';
+fcode_write_sr  = '06';
+device_id       = '03';
+
+reg_add_weather_region      = dec2hex(112);
+reg_add_station_location    = dec2hex(110);
+
+% Open serial interface
+open_serial_port( 'COM6', 19200, 8, 'even', 1 );
+
+% Read value from register address where the city id is stored. If values
+% differ, write new value to register.
+
+[modbus_pdu] = gen_pdu(device_id,fcode_read_sr,reg_add_weather_region,'0001');
+[ txdata_hex ] = crc_calc( modbus_pdu );
+[ txdata ] = format_modbus_msg( txdata_hex );
+
+
+[ CityID ] = get_city_id( CityName );
+
+
+end
+
--- open_serial_port.m
+++ open_serial_port.m
@@ -1,17 +1,17 @@
 function [ ] = open_serial_port( com_address, baudrate, databits, parity, stopbit )
 %This function establishes the modbus communication channel
 %   Detailed explanation goes here
-if size(com_address,2) < 4
-    errordlg('Bitte geben Sie eine gültige Com-Addresse ein!', 'Falscher Com-Port');
-elseif isnan(baudrate)
-    errordlg('Bitte wählen Sie eine gültige Baudrate aus!', 'Falsche Baudrate');
-elseif isempty(databits)
-    errordlg('Bitte geben Sie gültige Databits ein!', 'Falsche Databits');
-elseif isempty(parity)
-    errordlg('Bitte wählen Sie ein gültiges Parity aus!', 'Falsche Parity');
-elseif isempty(stopbit)
-    errordlg('Bitte geben Sie ein gültiges Stop-Bit ein', 'Falsches Stopbit');
-end
+% if size(com_address,2) < 4
+%     errordlg('Bitte geben Sie eine gültige Com-Addresse ein!', 'Falscher Com-Port');
+% elseif isnan(baudrate)
+%     errordlg('Bitte wählen Sie eine gültige Baudrate aus!', 'Falsche Baudrate');
+% elseif isempty(databits)
+%     errordlg('Bitte geben Sie gültige Databits ein!', 'Falsche Databits');
+% elseif isempty(parity)
+%     errordlg('Bitte wählen Sie ein gültiges Parity aus!', 'Falsche Parity');
+% elseif isempty(stopbit)
+%     errordlg('Bitte geben Sie ein gültiges Stop-Bit ein', 'Falsches Stopbit');
+% end
 serial_interface = serial(com_address,'BaudRate',baudrate,'DataBits',databits,'Parity',parity,'StopBits',stopbit);
 assignin('base','serial_interface',serial_interface);
 fopen(serial_interface);
