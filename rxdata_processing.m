58b5133e01bc1741cc3533a335d801722b82cc9c
Andreas Henneberger <andreas.henneberger@mytum.de>
2014-01-27 08:27:42 +0100

    rxdata exception

--- rxdata_processing.m
+++ rxdata_processing.m
@@ -1,17 +1,18 @@
 function [ response_data, crc_check_value, response_msg ] = ...
-           rxdata_processing( rxdata, modbus_msg, fc_def, res, con_qual, lng, lat )
+           rxdata_processing( rxdata, modbus_pdu, fc_def, res, con_qual, lng, lat )
 %Processing the received rxdata from serial interface
 %   Rxdata contains the response from the MODBUS server, which has to be
 %   processed here and in a subsequent function.
 
 not_done = 1;
+cnt = 0;
 
 while not_done == 1
     if isempty(rxdata)
-        error('No data received! Check if server is available!');
-%         response_data = [];
-%         crc_check_value = 0;
-%         response_msg = [];
+        error_msg = ('No data received! Check if server is available!');
+        response_data = [];
+        crc_check_value = 0;
+        response_msg = [];
     else
         func_code = dec2hex(rxdata(2),2);
         fcode_error = fcode_check(func_code);
@@ -20,22 +21,22 @@ while not_done == 1
             exception_code = dec2hex(rxdata(3),2);
             switch exception_code
                 case '01'
-                    error('Exception Code 01 -> Function code not supported');
+                    error_msg = ('Exception Code 01 -> Function code not supported');
                 case '02'
-                    error('Exception Code 02 -> Output address not valid');
+                    error_msg = ('Exception Code 02 -> Output address not valid');
                 case '03'
-                    error(['Exception Code 03 -> Quantity of outputs exceeds'...
+                    error_msg = (['Exception Code 03 -> Quantity of outputs exceeds'...
                            'range 0x0001 and 0x07D0']);
                 case '04'
-                    error(['Exception Code 04 -> Failure during reading discret'...
+                    error_msg = (['Exception Code 04 -> Failure during reading discret'...
                           'outputs']);
             end
-%             response_data = [];
-%             crc_check_value = 0;
-%             response_msg = [];
+            response_data = [];
+            crc_check_value = 0;
+            response_msg = [];
         else
 %             pause(0.25)
-%             err_msg = [];
+            error_msg = [];
             switch rxdata(2)
                 case 1
 %                     byte_count = rxdata(3);
@@ -56,8 +57,13 @@ while not_done == 1
         end
     end
 
-    if (isempty(response_data) || isempty(crc_check_value) || isempty(response_msg))
+    if (isempty(response_data) || isempty(crc_check_value) || isempty(response_msg)) && ~isempty(error_msg)
         not_done = 1;
+        send_and_receive_data(modbus_pdu, fc_def, res, con_qual, lng, lat);
+        cnt = cnt + 1;
+        if cnt == 3
+            error(error_msg);
+        end
     else
         not_done = 0;
     end
