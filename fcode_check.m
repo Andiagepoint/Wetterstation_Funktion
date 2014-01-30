c397843395e68e8357349defb2847d5840f1bd8c
Andreas Henneberger <andreas.henneberger@mytum.de>
2014-01-30 12:50:56 +0100

    version 3.8

--- fcode_check.m
+++ fcode_check.m
@@ -1,5 +1,5 @@
 function [ fcode_error ] = fcode_check( fcode )
-%UNTITLED2 Summary of this function goes here
+%Check whether function code is valid or not
 %   Detailed explanation goes here
 switch fcode
     case dec2hex(1,2)
